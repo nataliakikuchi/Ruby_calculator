80d3c8a9401ec83e78fead884e4a08afd725d54c
Natalia Kikuchi <nataliakikuchi90@gmail.com>
2020-07-09 19:09:22 -0300

    Implements correction in order to avoid repetitions

--- calculator.rb
+++ calculator.rb
@@ -1,25 +1,25 @@
 class Calculator
   def initialize(overrides = {})
-    @calculate_sum = overrides.fetch(:calculate_sum) { Sum.new }
-    @calculate_subtraction = overrides.fetch(:calculate_subtraction) { Subtraction.new }
-    @calculate_multiplication = overrides.fetch(:calculate_multiplication) { Multiplication.new }
-    @calculate_division = overrides.fetch(:calculate_division) { Division.new }
+    @sum = overrides.fetch(:sum) { Sum.new }
+    @subtraction = overrides.fetch(:subtraction) { Subtraction.new }
+    @multiplication = overrides.fetch(:multiplication) { Multiplication.new }
+    @division = overrides.fetch(:division) { Division.new }
   end
 
   def sum(number_1, number_2)
-    @calculate_sum.sum(number_1, number_2)
+    @sum.calculate(number_1, number_2)
   end
 
   def subtraction(number_1, number_2)
-    @calculate_subtraction.subtraction(number_1, number_2)
+    @subtraction.calculate(number_1, number_2)
   end
 
   def multiplication(number_1, number_2)
-    @calculate_multiplication.multiplication(number_1, number_2)
+    @multiplication.calculate(number_1, number_2)
   end
 
   def division(number_1, number_2)
-    @calculate_division.division(number_1, number_2)
+    @division.calculate(number_1, number_2)
   end
 
   def calculate(operator, number_1, number_2)
@@ -39,25 +39,25 @@ class Calculator
 end
 
 class Sum
-  def sum(number_1, number_2)
+  def calculate(number_1, number_2)
     number_1 + number_2
   end
 end
 
 class Subtraction
-  def subtraction(number_1, number_2)
+  def calculate(number_1, number_2)
     number_1 - number_2
   end
 end
 
 class Multiplication
-  def multiplication(number_1, number_2)
+  def calculate(number_1, number_2)
     number_1 * number_2
   end
 end
 
 class Division
-  def division(number_1, number_2)
+  def calculate(number_1, number_2)
     return number_1 / number_2 if number_2 > 0
     'Não é possível fazer divisão por zero.'
   end
